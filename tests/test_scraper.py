8a915b7f361dcde3f8a3c40fd1b1eae43fcbcdd9
Mustfa Ayyed Alhasanat <mustfaaayyed@gmail.com>
2022-04-14 11:51:15 +0300

    modifying 3

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,9 +1,17 @@
 from web_scraper.scraper import WebScraper
+import pytest
 
 
-def test_get_citations_needed_count():
-    ws = WebScraper("https://en.wikipedia.org/wiki/History_of_Mexico")
+def test_get_citations_needed_count(ws):
     assert ws.get_citations_needed_count() == 5
     
 
+def test_get_citations_needed_report():
+    with open('citations_needed_report.txt', 'r') as f:
+        assert "".join(f.readlines()).count("<p>") == 5
 
+
+@pytest.fixture
+def ws():
+    return WebScraper("https://en.wikipedia.org/wiki/History_of_Mexico")
+    
--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -21,18 +21,30 @@ class WebScraper:
         soup = BeautifulSoup(res.content, 'html.parser')
         paragraphs = soup.find_all('p')
         
-        self.citation_needed = []
+        self.citation_needed_paragraphs = []
         
         for p in paragraphs:
-            super_scripted = p.find_all('sup')    
-            for sup in super_scripted:
-                if sup:
-                    list_item = sup.find('i')
-                    if list_item:
-                        anchor = list_item.find('a')
-                        self.citation_needed.append(p)
-
-
+            super_scripted = p.find('sup')    
+            if super_scripted:
+                list_item = super_scripted.find('i')
+                if list_item:
+                    self.citation_needed_paragraphs.append(p)        
+        
+        self.citation_needed =[]
+        
+        for text in self.citation_needed_paragraphs:
+            num_of_citations = str(text).count(">citation needed<")
+            
+            start_point = 0
+            for i in range(num_of_citations):
+                citation_index = str(text).find(">citation needed<", start_point)
+                citation = str(text)[start_point : citation_index+16]
+                start_point = citation_index+16
+                if citation.count("<p>") == 0:
+                    citation = "<p>" + citation[23:]
+                self.citation_needed.append(citation)
+            
+                        
         return len(self.citation_needed)
 
         
@@ -48,7 +60,7 @@ class WebScraper:
         counter = 1
 
         for text in self.citation_needed:
-            self.report[f"Paragraph {counter}: "] = f"{str(text)[3:-4]}"
+            self.report[f"Paragraph {counter}: "] = f"{str(text)}\n"
             counter += 1
             
         return self.report
@@ -68,6 +80,7 @@ class WebScraper:
             
     
     
-ws = WebScraper("https://en.wikipedia.org/wiki/History_of_Mexico")
-ws.save_results()
+if __name__ == "__main__":
+    ws = WebScraper("https://en.wikipedia.org/wiki/History_of_Mexico")
+    ws.save_results()
     
